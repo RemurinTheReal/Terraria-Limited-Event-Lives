b9815b53c76a14e0308a32456490f8544250e150
Remurin <73508129+RemurinTheReal@users.noreply.github.com>
2025-12-31 08:27:31 -0800

    Initial commit

---
+++ LimitedEventLivesPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace LimitedEventLives;
+
+public sealed class LimitedEventLivesPlayer : ModPlayer
+{
+    public override void UpdateDead()
+    {
+        if (Main.netMode == NetmodeID.MultiplayerClient) return;
+        if (Main.invasionType == 0) return;
+        
+        if (Main.PlayerList.Any(player => !player.Player.dead)) return;
+        
+        Main.invasionDelay = 0;
+        Main.invasionType = 0;
+        Main.invasionSize = 0;
+        Main.invasionWarn = 0;
+        Main.invasionX = 0.0;
+        Main.invasionSizeStart = 0;
+    }
+}
